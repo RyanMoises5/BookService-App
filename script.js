44dfc3d5e78da8f0d5e6e98bbbabe99d684067e3
aaron brooks <aaron.brooks123@gmail.com>
2024-02-05 20:54:58 -0500

    pushing new changes

---
+++ script.js
@@ -0,0 +1,27 @@
+const bookNames = [
+    "The Art Of War",
+    "48 Laws of Power",
+    "The Great Gatsby",
+   
+];
+
+const searchInput = document.getElementById("search-input");
+const searchResults = document.getElementById("search-results");
+localStorage.setItem("myBooks", "")
+
+searchInput.addEventListener("input", handleSearch);
+
+function handleSearch() {
+    const searchTerm = searchInput.value.toLowerCase();
+    const filteredBooks = bookNames.filter(book => book.toLowerCase().includes(searchTerm));
+
+    
+    searchResults.innerHTML = "";
+
+  
+    filteredBooks.forEach(book => {
+        const li = document.createElement("li");
+        li.textContent = book;
+        searchResults.appendChild(li);
+    });
+}
