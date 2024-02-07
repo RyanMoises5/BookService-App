d67d3ad4d8e0aaa48bba49e9f4c204e41163e29d
Ryan M <ryan.a.moises@gmail.com>
2024-02-06 19:37:25 -0500

    Display results now work

--- assets/APIs-work.js
+++ assets/APIs-work.js
@@ -1,7 +1,7 @@
 var searchTerm = $("#search-input");
 var submitBtn = $('#submit-button');
 var googleAPIKey = "AIzaSyApC_HmnBEAvRsbJdOI__0QIfXjbtebdwY";
-var searchResults = $('#search-results');
+var searchResults = $("#search-results");
 
 function formSubmission (event) {
     event.preventDefault()
@@ -42,20 +42,25 @@ var getData = function (searchInput) {
 };
 
 var displayResults = function(data) {
+
     for (let index = 0; index < 10; index++) {
 
         var resultsCard = $('<div>');
 
         var bookTitle = $('<h2>');
         bookTitle.text("Title: " + data.docs[index].title);
+
         var bookAuthor = $('<h3>');
         bookAuthor.text("Author: " + data.docs[index].author_name);
+
         var bookBorrow = $('<p>');
         bookBorrow.text(data.docs[index].ebook_access)
 
         resultsCard.append(bookTitle);
         resultsCard.append(bookAuthor);
         resultsCard.append(bookBorrow);
+
+        console.log(bookAuthor);
 
         searchResults.append(resultsCard);
     }
